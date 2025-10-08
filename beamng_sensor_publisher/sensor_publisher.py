9d8450a500c393b8034a0cf527b0b5f7de089ac9
William Ward <willward1912@gmail.com>
2025-10-08 10:02:39 -0500

    Add Damage msg.

--- beamng_sensor_publisher/sensor_publisher.py
+++ beamng_sensor_publisher/sensor_publisher.py
@@ -5,6 +5,8 @@ from beamngpy import BeamNGpy, Scenario, Vehicle
 from beamngpy.sensors import *
 
 from beamng_msgs.msg import StateSensor, VehicleControl
+from beamng_sensor_publisher.msg import DamageTotal
+
 
 
 class BeamNGBridge(Node):
@@ -38,6 +40,7 @@ class BeamNGBridge(Node):
         # Publisher
         self.state_pub = self.create_publisher(StateSensor, 'beamng/state', 10)
         self.control_pub = self.create_publisher(VehicleControl, 'beamng/vehicle_control', 10)
+        self.damage_pub = self.create_publisher(DamageTotal, 'beamng/damage_total', 10)
 
         # Timer to poll + publish
         self.timer = self.create_timer(0.1, self.publish_state)  # 10 Hz
@@ -66,9 +69,17 @@ class BeamNGBridge(Node):
         vc.parkingbrake = elec['parkingbrake']
         vc.clutch = elec['clutch']
 
+        # Fill the damaga ROS message
+        damage_total = DamageTotal()
+        damage_total.lowpressure = damage['lowpressure']
+        damage_total.damage = damage['damage']
+        damage_total.part_damage = damage['part_damage']
+        damage_total.damage_ext = damage['damage_ext']
+
         # Publish
         self.state_pub.publish(msg)
         self.control_pub.publish(vc)
+        self.damage_pub.publish(damage_total)
         # self.get_logger().info(f"Published state at pos={msg.position.x:.2f},{msg.position.y:.2f}")
 
 
